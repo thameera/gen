a60b136a030dbface6ce7fcf8e4c8ba59e248bdd
Thameera Senanayaka <me@thameera.com>
2020-08-15 20:47:28 +1000

    Integrate material-ui

---
+++ components/GenButtons.js
@@ -0,0 +1,9 @@
+import { Button } from '@material-ui/core'
+
+export default function GenButtons() {
+  return (
+    <>
+      <Button variant="contained" color="primary">Generate tmp.html</Button>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import Head from 'next/head'
+import { Container, Box } from '@material-ui/core'
+
+import GenButtons from '../components/GenButtons'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>Flow generator</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
 
-      <main className={styles.main}>
-        Gen
-      </main>
-    </div>
+      <Box>
+        <h1>Gen</h1>
+      </Box>
+      <Box>
+        <GenButtons />
+      </Box>
+    </Container>
   )
 }
