42c4b1f08c6bee6fb3987f327e66d67e5dffa7da
Thameera Senanayaka <me@thameera.com>
2023-08-23 22:12:54 +1000

    Create ActionsProvider

---
+++ components/actions/ActionsProvider.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+export const ActionsContext = createContext()
+
+export const ActionsProvider = ({ tenant, children }) => {
+  const [triggers, setTriggers] = useState([])
+
+  useEffect(async () => {
+    const res = await axios(`/api/mgmt/actions/getActions?tenant=${tenant}`)
+    setTriggers(res.data)
+  }, [])
+
+  return (
+    <ActionsContext.Provider value={{ triggers, setTriggers }}>
+      {children}
+    </ActionsContext.Provider>
+  )
+}
+
+export const useActionsContext = () => {
+  const context = useContext(ActionsContext)
+  if (context === undefined) {
+    throw new Error('useActionsContext must be used within a ActionsProvider')
+  }
+  return context
+}
