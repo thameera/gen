8c6f75899a956f2d243e7d36b982944871d4329e
Thameera Senanayaka <me@thameera.com>
2023-12-13 23:45:26 +1100

    Fix error when custom_login_page_on is undefined

--- pages/api/mgmt/prompts/patchPrompts.js
+++ pages/api/mgmt/prompts/patchPrompts.js
@@ -16,18 +16,10 @@ export default async (req, res) => {
     res.end('No ULP experience specified')
     return
   }
-  if (
-    req.body.custom_login_page_on === null ||
-    req.body.custom_login_page_on === undefined
-  ) {
-    res.statusCode = 400
-    res.end('No custom login page status specified')
-    return
-  }
 
   const data = {
     universal_login_experience: req.body.universal_login_experience,
-    custom_login_page_on: req.body.custom_login_page_on,
+    custom_login_page_on: req.body.custom_login_page_on || false,
     identifier_first: req.body.identifier_first,
     webauthn_platform_first_factor: req.body.webauthn_platform_first_factor,
   }
